0b3330fd005680d72970d608cdaf5dfa971d30cf
ammar0611 <ammarmansuri1826@gmail.com>
2025-04-07 08:23:44 +0530

    first commit

---
+++ attende/App.js
@@ -0,0 +1,157 @@
+import React, { useState, useEffect } from "react";
+import Loginstudent from "./src/screens/Loginstudent";
+import Loginteacher from "./src/screens/Loginteacher";
+import Welcome from "./src/screens/Welcome";
+import colors from "./config/colors";
+import Teacherhome from "./src/screens/Teacherhome";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Takeattendance from "./src/screens/TakeAttendance";
+import Studenthome from "./src/screens/Studenthome";
+import AttendanceRecord from "./src/screens/AttendanceRecord";
+import * as SecureStore from "expo-secure-store";
+import Attendanceinfo from "./src/screens/Attendanceinfo";
+import { LoaderProvider, useLoader } from "./src/context/LoaderContext";
+import Loader from "./src/components/Loader";
+import {
+  useFonts,
+  Poppins_800ExtraBold,
+  Poppins_100Thin,
+  Poppins_500Medium,
+  Poppins_400Regular,
+  Poppins_300Light,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
+
+const Stack = createNativeStackNavigator();
+
+const AppContent = () => {
+  const { loading } = useLoader();
+  const [date, setDate] = useState(null);
+  const [defaultScreen, setDefaultScreen] = useState("Welcome");
+
+  const getToken = async () => {
+    const token = await SecureStore.getItemAsync("user");
+    if (token) {
+      console.log(token);
+      const role = JSON.parse(token).role;
+      if (role === "student") {
+        setDefaultScreen("Studenthome");
+      } else {
+        setDefaultScreen("Teacherhome");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+    let today = new Date();
+    let date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    setDate(date);
+  }, []);
+
+  let [fontsLoaded] = useFonts({
+    Poppins_800ExtraBold,
+    Poppins_500Medium,
+    Poppins_100Thin,
+    Poppins_400Regular,
+    Poppins_300Light,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <Loader visible={loading} />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={defaultScreen}>
+          <Stack.Screen
+            name="Attendance"
+            component={Takeattendance}
+            options={{
+              title: "Attendance: " + date,
+              headerStyle: {
+                backgroundColor: colors.primary,
+              },
+              headerTintColor: colors.white,
+            }}
+          />
+
+          <>
+            <Stack.Screen
+              name="Welcome"
+              component={Welcome}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Logins"
+              component={Loginstudent}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Logint"
+              component={Loginteacher}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </>
+          <Stack.Screen
+            name="Teacherhome"
+            component={Teacherhome}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Studenthome"
+            component={Studenthome}
+            options={{
+              headerShown: false,
+            }}
+          />
+
+          <Stack.Screen
+            name="Attendancerecord"
+            component={AttendanceRecord}
+            options={{
+              title: "Attendance record",
+              headerStyle: {
+                backgroundColor: colors.primary,
+              },
+              headerTintColor: colors.white,
+            }}
+          />
+          <Stack.Screen
+            name="Studentinfo"
+            component={Attendanceinfo}
+            options={{
+              title: "",
+              headerTransparent: true,
+              headerTintColor: colors.white,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <LoaderProvider>
+      <AppContent />
+    </LoaderProvider>
+  );
+}
---
+++ attende/src/screens/TakeAttendance.js
@@ -0,0 +1,144 @@
+import styles from "../styles/take_attendance";
+import { ScrollView, SafeAreaView, View, Alert, LogBox } from "react-native";
+import StudentBox from "../components/StudentBox";
+import { faAngleDoubleRight, faTimes } from "@fortawesome/free-solid-svg-icons";
+import colors from "../../config/colors";
+import { useState, useEffect } from "react";
+import Button from "../components/Button";
+import {
+  getDocs,
+  collection,
+  query,
+  where,
+  addDoc,
+  Timestamp,
+  orderBy,
+} from "firebase/firestore";
+import { db } from "../../config/firebase";
+import AttendanceModal from "../components/AttendanceModal";
+import { useLoading } from "../hooks/useLoading";
+
+const Attendance = ({ navigation }) => {
+  const [showModal, setShowModal] = useState(false); // control modal visibility
+  const [students, setStudents] = useState([]); // array of student objects
+  const [presentStudents, setPresentStudents] = useState([]); // array of present student ids
+  const { withLoading } = useLoading();
+
+  const getStudents = async () => {
+    try {
+      await withLoading(async () => {
+        // query to fetch only student data
+        const q = query(
+          collection(db, "users"),
+          where("role", "==", "student"),
+          orderBy("roll_no", "asc")
+        );
+
+        // fetching all the documents
+        const querySnapshot = await getDocs(q);
+
+        // extracting the data from the documents
+        const items = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        // setting the students
+        setStudents(items);
+
+        //get id of all students
+        const ids = items.map((student) => student.id);
+
+        // set the present students
+        setPresentStudents(ids);
+      });
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "Failed to fetch students", [
+        {
+          text: "OK",
+        },
+      ]);
+    }
+  };
+
+  const handleSubmit = async () => {
+    // make an array of absent student ids
+    const absentStudents = students
+      .map((student) => student.id)
+      .filter((id) => !presentStudents.includes(id));
+
+    // update the attendance in the database
+
+    try {
+      await withLoading(async () => {
+        await addDoc(collection(db, "attendance"), {
+          presentStudents,
+          absentStudents,
+          date: Timestamp.fromDate(new Date()),
+        });
+
+        // show success message
+        Alert.alert("Success", "Attendance has been marked");
+        navigation.navigate("Teacherhome");
+      });
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "Failed to save attendance", [
+        {
+          text: "OK",
+        },
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    // get the students
+    getStudents();
+
+    // Ignore the warning about the setting a timer
+    LogBox.ignoreLogs(["Setting a timer"]);
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {students.map((student) => (
+          <StudentBox
+            key={student.id}
+            setPresentStudents={setPresentStudents}
+            presentStudents={presentStudents}
+            item={student}
+          />
+        ))}
+
+        <View style={styles.buttons}>
+          <Button
+            onPress={() => navigation.navigate("Teacherhome")}
+            text="Cancel"
+            icon={faTimes}
+            color={colors.red}
+            styles={styles}
+          />
+          <Button
+            onPress={() => setShowModal(true)}
+            text="Next"
+            icon={faAngleDoubleRight}
+            color={colors.primary}
+            styles={styles}
+          />
+        </View>
+      </ScrollView>
+
+      <AttendanceModal
+        handleSubmit={handleSubmit}
+        presentStudents={presentStudents}
+        visible={showModal}
+        setShowModal={setShowModal}
+        allStudentsId={students.map((student) => student.id)}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Attendance;
